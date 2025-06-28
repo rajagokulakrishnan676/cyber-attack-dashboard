4e6c1265630377dd05a31f05ba634de9a509dcaf
rajagokulakrishnan676 <rajagokulakrishnan676@gmail.com>
2025-06-28 18:02:04 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/cyber-attack-dashboard/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
